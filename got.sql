c4afec0957882f2f74ebb0837c67e4d36e95afe4
WSR <test@test.ru>
2020-02-26 09:55:20 +0400

    init

---
+++ got.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 26 2020 г., 08:54
+-- Версия сервера: 5.7.25-log
+-- Версия PHP: 7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `got`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `great_houses`
+--
+
+CREATE TABLE `great_houses` (
+  `id` int(11) NOT NULL,
+  `house` text COLLATE utf8_bin NOT NULL,
+  `img` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Дамп данных таблицы `great_houses`
+--
+
+INSERT INTO `great_houses` (`id`, `house`, `img`) VALUES
+(1, 'Ланнистеры', 'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest?cb=20170101095357'),
+(2, 'Баратеоны', 'https://vignette.wikia.nocookie.net/gameofthrones/images/0/00/House-Baratheon-Main-Shield.PNG/revision/latest/scale-to-width-down/100?cb=20170519002924');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `threads_links`
+--
+
+CREATE TABLE `threads_links` (
+  `user_id` int(11) NOT NULL,
+  `thread_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `threards`
+--
+
+CREATE TABLE `threards` (
+  `id` int(11) NOT NULL,
+  `title` text COLLATE utf8_bin NOT NULL,
+  `text` text COLLATE utf8_bin NOT NULL,
+  `date_post` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `verified` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `user_id` int(11) NOT NULL,
+  `token` varchar(32) COLLATE utf8_bin NOT NULL,
+  `expiration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` text COLLATE utf8_bin NOT NULL,
+  `password_hash` text COLLATE utf8_bin NOT NULL,
+  `permissions` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `great_houses`
+--
+ALTER TABLE `great_houses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `house` (`house`(32));
+
+--
+-- Индексы таблицы `threads_links`
+--
+ALTER TABLE `threads_links`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `thread_id` (`thread_id`);
+
+--
+-- Индексы таблицы `threards`
+--
+ALTER TABLE `threards`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `great_houses`
+--
+ALTER TABLE `great_houses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `threards`
+--
+ALTER TABLE `threards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
