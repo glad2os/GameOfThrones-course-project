1449a13e22a034bf6360d7d00a213dcf60824098
gladdos <lite.console4bit@gmail.com>
2020-03-06 23:51:41 +0400

    fix

--- engine/api/threads/get_news.php
+++ engine/api/threads/get_news.php
@@ -1,28 +1,20 @@
 <?php
-use Config\Books;
-use Exception\IllegalArgumentException;
+
 use Database\MySQLi;
 
 $request = json_decode(file_get_contents("php://input"), true);
 
-//if (!isset($request['page']))
-//    throw new IllegalArgumentException("Field 'page' must be set");
-
 $mysqli = new MySQLi();
 
-$books = $mysqli->getAllThreads();
+$threads = $mysqli->getAllThreads();
 
-foreach ($books as $key => $book) {
-    $books[$key]['authors'] = $mysqli->getAuthorsOfThread($book['id']);
+foreach ($threads as $key => $thread) {
+    $threads[$key]['authors'] = $mysqli->getAuthorsOfThread($thread['id']);
 }
-print json_encode($books);
 
-if (sizeof($books) == 0) {
+if (sizeof($threads) == 0) {
     http_response_code(204);
     exit();
 }
-print json_encode([
-    'page' => $request['page'],
-    'pageCount' => ceil($mysqli->countOfBooks() / Books\BOOKS_ON_PAGE),
-    'books' => $books
-]);
+
+print json_encode($threads);
